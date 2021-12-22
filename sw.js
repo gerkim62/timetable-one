3d2de6dc4ed855002f91f628ea3eb5c451f86ee4
gerkim62 <72649107+gerkim62@users.noreply.github.com>
2021-12-22 21:32:15 +0300

    Create sw.js

--- sw.js
+++ sw.js
@@ -3,11 +3,11 @@ const toCache = [
      "/",/*
   "https://fonts.googleapis.com",
   "https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700&display=swap",
-  "https://fonts.gstatic.com",*/
+  "https://fonts.gstatic.com",
   "https://cdn.jsdelivr.net/npm/pretty-checkbox@3.0/dist/pretty-checkbox.min.css",
   "https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css",
    "https://cdn.jsdelivr.net/npm/toastify-js"
-  ,
+  ,*/
   'icon-512x512.png',
   'icon-192x192.png',
   "https://cdn.jsdelivr.net/npm/toastify-js",
